56cca9faa0bd9f429437d3cc50010f1c32057a61
radosimarmata <simarmatarado@gmail.com>
2021-07-10 11:15:34 +0700

    Signature

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import SignaturePad from 'signature_pad';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  signaturePad: SignaturePad;
+  @ViewChild('canvas') canvasEl : ElementRef;
+  signatureImg: string;
 
-  constructor() {}
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.signaturePad = new SignaturePad(this.canvasEl.nativeElement);
+  }
+
+  startDrawing(event: Event) {
+    console.log(event);
+    // works in device not in browser
+
+  }
+
+  moved(event: Event) {
+    // works in device not in browser
+  }
+
+  clearPad() {
+    this.signaturePad.clear();
+  }
+
+  savePad() {
+    const base64Data = this.signaturePad.toDataURL();
+    this.signatureImg = base64Data;
+  }
 
 }
